088eaad62caa4a1e34d7e6bef2721d4fdff5469a
Daniel Harris <daniel.harris@dpi.nsw.gov.au>
2017-01-31 15:13:54 +1100

    Area sum and outlines for some other calcs.

--- org-process.py
+++ org-process.py
@@ -35,9 +35,38 @@ def get_data_from_report(file):
     ends = sheet.col_values(opx.PEAK_END_COLUMN, PEAK_LIST_START_ROW, PEAK_LIST_END_ROW)
     areas = sheet.col_values(opx.AREA_COLUMN, PEAK_LIST_START_ROW, PEAK_LIST_END_ROW)
 
+    peaks = map(int, peaks)
     peak_data = zip(peaks, starts, rts, ends, areas)
 
     return sample_name, analysis_time, peak_data
+
+
+def get_bounding_indexes():
+    # TODO: get_bounding_indexes()
+    pass
+
+
+def sum_areas(peak_data_list, low_index, high_index):
+    """
+    Sums the peak areas given a set of bounding indices for the peak data list
+    :param peak_data_list: Peak area list for the sample
+    :param low_index: Index of first peak to sum
+    :param high_index: Index of final peak in list to sum
+    :return: Total area between the bounding indices.
+    """
+    areas = [x[4] for x in peak_data_list[low_index:high_index + 1]]
+    return sum(areas)
+
+
+def get_istd_area(peak_data_list, istd_rt):
+    """
+    Get the peak area for the given internal standard
+    :param peak_data_list: Peak area list for the sample
+    :param istd_rt: Retention time for the given internal standard
+    :return: Peak area integration for the internal standard
+    """
+    # TODO: get_istd_area(peak_data_list, istd_rt)
+    pass
 
 
 def main():
@@ -45,6 +74,7 @@ def main():
     FILE = r'C:\code\projects\org-process\extras\analysis-reports\16_0475_1_AnalysisReport.xls'
     sample_name, analysis_time, peak_data = get_data_from_report(FILE)
     print peak_data
+    print sum_areas(peak_data, 0, 29)
 
 
 if __name__ == "__main__":
