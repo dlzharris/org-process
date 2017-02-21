6b65bc7bd834c8869a97e2ff2555dec3b766a5a5
Daniel Harris <daniel.harris@dpi.nsw.gov.au>
2017-02-21 15:46:06 +1100

    Perform calculations.

--- org-process.py
+++ org-process.py
@@ -51,19 +51,16 @@ def check_dir_validity(dir_path):
 
 def main():
     """Run the org-process app."""
-    FILE = r'C:\code\projects\org-process\extras\analysis-reports\16_0475_1_AnalysisReport.xls'
-    sample_name, analysis_time, peak_data = get_data_from_report(FILE)
-
     # User inputs
 
     # Make list of all blank files in directory
-    # Make list of all sample files in directory (includes QC)
     dir = r'C:\code\projects\org-process\extras\analysis-reports'
     blank_data_list = []
     blank_pattern = dir + '\*' + opx.BLANK_TAG + '*'
     blank_file_list = glob.glob(blank_pattern)
+
     for f in blank_file_list:
-        blank = get_data_from_report(f)
+        blank = get_data_from_report(f)[2]
         blank_data_list.append(blank)
 
     blank_average = op.BlankAverage(
@@ -73,13 +70,79 @@ def main():
         istd_area_target=opx.DEF_ISTD_AREA_TARGET,
         istd_area_tolerance=opx.DEF_ISTD_AREA_TOLERANCE)
 
-    # For each file in BLANKS
-    #    blank = get_data_from_report(file)
-    #    blank_data_list.append(blank)
-    # blank_average = op.BlankAverage(all the stuff)
+    # Make list of all sample files in directory (includes QC)
+    sample_file_list = [x for x in glob.glob(dir + '\*') if x not in blank_file_list]
 
-    # for each sample file in selected directory
-        # calculate sample concentration
+    # Iterate through sample files and calculate results
+    for f in sample_file_list:
+        sample_name, analysis_time, peak_data = get_data_from_report(f)
+
+        # Calculate C6-C10
+        if opx.DEF_ANALYSIS_C6_C10:
+            i_c6 = 1
+            i_c10 = op.get_fraction_end_index(peak_data, opx.C6_C10_END)
+            conc_c6_c10 = op.calculate_sample_concentration(
+                    peak_data_list=peak_data,
+                    blank=blank_average,
+                    low_index=i_c6,
+                    high_index=i_c10,
+                    istd_rt=opx.DEF_IST_RT,  # USER INPUT
+                    istd_rt_tolerance=opx.DEF_ISTD_RT_TOLERANCE,  # USER INPUT
+                    istd_area_target=opx.DEF_ISTD_AREA_TARGET,  # USER INPUT
+                    istd_area_tolerance=opx.DEF_ISTD_AREA_TOLERANCE,  # USER INPUT
+                    calibration_slope=4.2608,  # USER INPUT
+                    calibration_intercept=0,  # USER INPUT
+                    istd_concentration=opx.DEF_ISTD_CONC_C6_C10,  # USER INPUT
+                    dilution_factor=opx.DEF_DILUTION_FACTOR_C6_C10)  # USER INPUT
+
+        # Calculate C10-C16
+        if not opx.DEF_ANALYSIS_C6_C10:
+            i_c16 = op.get_fraction_end_index(peak_data, opx.C10_C16_END)
+            conc_c6_c10 = op.calculate_sample_concentration(
+                peak_data_list=peak_data,
+                blank=blank_average,
+                low_index=i_c10,
+                high_index=i_c16,
+                istd_rt=opx.DEF_IST_RT,  # USER INPUT
+                istd_rt_tolerance=opx.DEF_ISTD_RT_TOLERANCE,  # USER INPUT
+                istd_area_target=opx.DEF_ISTD_AREA_TARGET,  # USER INPUT
+                istd_area_tolerance=opx.DEF_ISTD_AREA_TOLERANCE,  # USER INPUT
+                calibration_slope=4.2608,  # USER INPUT
+                calibration_intercept=0,  # USER INPUT
+                istd_concentration=opx.DEF_ISTD_CONC_C10_C40,  # USER INPUT
+                dilution_factor=opx.DEF_DILUTION_FACTOR_C10_C40)  # USER INPUT
+
+            # Calculate C16-C34
+            i_c34 = op.get_fraction_end_index(peak_data, opx.C16_C34_END)
+            conc_c16_c34 = op.calculate_sample_concentration(
+                peak_data_list=peak_data,
+                blank=blank_average,
+                low_index=i_c16,
+                high_index=i_c34,
+                istd_rt=opx.DEF_IST_RT,  # USER INPUT
+                istd_rt_tolerance=opx.DEF_ISTD_RT_TOLERANCE,  # USER INPUT
+                istd_area_target=opx.DEF_ISTD_AREA_TARGET,  # USER INPUT
+                istd_area_tolerance=opx.DEF_ISTD_AREA_TOLERANCE,  # USER INPUT
+                calibration_slope=4.2608,  # USER INPUT
+                calibration_intercept=0,  # USER INPUT
+                istd_concentration=opx.DEF_ISTD_CONC_C10_C40,  # USER INPUT
+                dilution_factor=opx.DEF_DILUTION_FACTOR_C10_C40)  # USER INPUT
+
+            # Calculate C34-C40
+            i_c40 = op.get_fraction_end_index(peak_data, opx.C34_C40_END)
+            conc_c34_c40 = op.calculate_sample_concentration(
+                peak_data_list=peak_data,
+                blank=blank_average,
+                low_index=i_c34,
+                high_index=i_c40,
+                istd_rt=opx.DEF_IST_RT,  # USER INPUT
+                istd_rt_tolerance=opx.DEF_ISTD_RT_TOLERANCE,  # USER INPUT
+                istd_area_target=opx.DEF_ISTD_AREA_TARGET,  # USER INPUT
+                istd_area_tolerance=opx.DEF_ISTD_AREA_TOLERANCE,  # USER INPUT
+                calibration_slope=4.2608,  # USER INPUT
+                calibration_intercept=0,  # USER INPUT
+                istd_concentration=opx.DEF_ISTD_CONC_C10_C40,  # USER INPUT
+                dilution_factor=opx.DEF_DILUTION_FACTOR_C10_C40)  # USER INPUT
 
 
 
